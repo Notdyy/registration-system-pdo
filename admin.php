b296578ff0d368bc77fc6c55994506f39f4a80d7
Nawaphon Makornsirisri <Notdyza1@hotmail.com>
2023-07-30 00:34:44 +0700

    edit test

--- admin.php
+++ admin.php
@@ -4,7 +4,7 @@ session_start();
 require_once 'config/db.php';
 if (!isset($_SESSION['admin_login'])) {
     $_SESSION['error'] = 'กรุณาเข้าสู่ระบบ!';
-    header('location: signin.php');
+    header('location: signin.php'); 
 }
 
 ?>
